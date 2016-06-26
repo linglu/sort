086c9c1f7451393e5ba9a85ac0b25ef4858bcbd7
linglu <893946347@qq.com>
2016-06-26 17:18:16 +0800

    优化插入排序算法

--- com/linky/InsertionSort.java
+++ com/linky/InsertionSort.java
@@ -17,36 +17,18 @@ public class InsertionSort {
     	 * 当前索引左边的所有元素都是有序的；
     	 * 当索引到达数组右端时，数组排序就完成了；
     	 */
-    	if (items.length < 2) {
-    		return;
-    	}
-    	
-    	for (int k = START_INDEX; k < items.length; k++) {
-    		SortItem curItem = items[k];
-    		
-    		// 如果当前的 item 的值是最小或者最大的；
-    		if (curItem.lessThan(items[0])) {
-    			moveToRight(0, k);
-    		} else if (items[k-1].lessThan(curItem)) {
-    			continue;
+    	int N = items.length;
+    	for (int i = 1; i < N; i++) {
+    		for (int j = i; j > 0 && items[j].lessThan(items[j-1]); j--) {
+    			exchange(j, j-1);
     		}
-    		
-    		// 如果当前的 item 值大小介于两者之间；
-	    	for(int i = 0; i < k; i++) {
-	    		if (items[i].lessThan(curItem) && curItem.lessThan(items[i+1])) {
-	    			// 从 i+1 开始到 curIndex-1，所有元素都向右移动一位 
-	    			moveToRight(i+1, k);
-	    		}
-	    	}
     	}
     }
     
-    public void moveToRight(int start, int end){
-    	SortItem item = items[end];
-    	for (int i = end; i > start; i--) {
-			items[i] = items[i - 1];
-		}
-		items[start] =  item;
+    private void exchange(int i, int j) {
+        int sorter = items[i].sorter;
+        items[i].sorter= items[j].sorter;
+        items[j].sorter = sorter;
     }
     
     public void display() {
